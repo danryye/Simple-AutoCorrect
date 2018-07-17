a492ac9a76d3e461604018c936f39b577010362b
BuildTools <unconfigured@null.spigotmc.org>
2018-07-16 17:14:22 -0700

    Added documentation and fixed out of bounds error during correction

--- src/AutoCorrection.java
+++ src/AutoCorrection.java
@@ -10,35 +10,49 @@ import java.util.List;
 public class AutoCorrection {
 	
 	public static void main(String[] args){
-		
+		/* Path to the desired text file. In this case, the file
+		   was named sample.txt and was located on my Desktop.*/
 		String filePath = "C:\\Users\\dan\\Desktop\\sample.txt";
 		
+		
+		System.out.println("Reading from file");
 		List<String> l = readFromFile(filePath);
 		
-		System.out.println("Read from file");
 		
+		System.out.println("Finished reading from file");
+		//Using the Iterator to print the contents from the raw
+		//text file
 		Iterator<String> itr = l.iterator();
 		while(itr.hasNext()){
 			System.out.println(itr.next());
 		}
 		
-		System.out.println("Applying Corrections.");
 		
+		System.out.println("Applying Corrections.");
 		String text = doCorrections(l);
+		
 		
 		System.out.println("Printing Corrected Text.");
 		System.out.println(text);
 		
+		
 		System.out.println("Finished");
 	}
 	
+	/*
+	 * Takes in the path of a text file and returns a list of 
+	 * type String. The contents of the text file will be in 
+	 * the zeroth index
+	 */
 	public static List<String> readFromFile(String filePath){
 		
-		List<String> list = Collections.emptyList();
+		List<String> list = Collections.emptyList(); //Creates empty list of type String
 		
+		//tries to retrieve the contents of the file from the attacked path
+		//using the Files class at java.nio.file.Files
 		try{
-			list = Files.readAllLines(Paths.get(filePath), StandardCharsets.UTF_8);
-		}catch(IOException e){
+			list = Files.readAllLines(Paths.get(filePath), StandardCharsets.UTF_8); //readAllLines(Paths, StandardCharsets)
+		}catch(IOException e){ //otherwise print the error
 			e.printStackTrace();
 		}
 		
@@ -46,12 +60,15 @@ public class AutoCorrection {
 		
 	}
 	
+	//Takes in a List of type String and applies corrections to the contents.
+	//Returns a String containing the corrected content.
+	//Currently, capitalizes the start of a sentence
 	public static String doCorrections(List<String> list){
 		String text = list.get(0);
-		text = text.substring(0, 1).toUpperCase() + text.substring(1);
-		for(int i = 1; i < text.length(); i++){
-			if(text.charAt(i) == '.' || text.charAt(i) == '!' || text.charAt(i) == '?'){
-				if(text.charAt(i+1) == ' '){
+		text = text.substring(0, 1).toUpperCase() + text.substring(1); //capitalizes first character
+		for(int i = 1; i < text.length(); i++){ //loop to go through entire content
+			if(text.charAt(i) == '.' || text.charAt(i) == '!' || text.charAt(i) == '?'){ //indicators of the end of a sentence
+				if(i + 1 < text.length() && i + 2 < text.length() && text.charAt(i+1) == ' '){
 					text = text.substring(0, i + 2) + text.substring(i + 2, i + 3).toUpperCase() + text.substring(i + 3);
 				}
 			}
